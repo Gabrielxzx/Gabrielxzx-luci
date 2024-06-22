496d244be5042268ef8b8fcade35f413d3381525
DESKTOP-ZX\10604 <gabrielx_zx@163.com>
2024-06-23 07:01:15 +0800

    init

--- files/scripts/tem-script.sh
+++ files/scripts/tem-script.sh
@@ -5,6 +5,5 @@ echo "This is tem_script start"
 rm -rf feeds/packages/net/uugamebooster
 cp -rf package/Gabrielxzx/packages/net/uugamebooster feeds/packages/net
 
-sed -i "s/DEPENDS:=+kmod-ipt-fullconenat/DEPENDS:=+iptables +kmod-ipt-fullconenat/g" package/network/services/fullconenat/Makefile
 
 echo "This is tem_script finish"
