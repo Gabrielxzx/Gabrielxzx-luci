baa0887f410c8b3e28dbc426f8d75e92e7d04b1f
DESKTOP-ZX\10604 <gabrielx_zx@163.com>
2024-06-23 01:46:50 +0800

    fix fullconenat

--- files/scripts/tem-script.sh
+++ files/scripts/tem-script.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-echo "This is tem_script"
+echo "This is tem_script start"
 
 rm -rf feeds/packages/net/uugamebooster
 cp -rf package/Gabrielxzx/packages/net/uugamebooster feeds/packages/net
+
+sed -i "s/DEPENDS:=+kmod-ipt-fullconenat/DEPENDS:=+iptables +kmod-ipt-fullconenat/g" package/network/services/fullconenat/Makefile
+
+echo "This is tem_script finish"
