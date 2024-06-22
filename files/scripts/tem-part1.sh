6ce993d3faf1b007a3a751fbcb73ae29bec8cd4e
DESKTOP-ZX\10604 <gabrielx_zx@163.com>
2024-06-23 07:41:12 +0800

    fix error

--- files/scripts/tem-part1.sh
+++ files/scripts/tem-part1.sh
@@ -4,6 +4,7 @@ echo "This is tem_script_part1 start"
 
 rm -rf feeds/packages/net/uugamebooster
 cp -rf package/Gabrielxzx/packages/net/uugamebooster feeds/packages/net
+rm -rf package/Gabrielxzx/packages/net/uugamebooster
 
 
 echo "This is tem_script_part1 finish"
