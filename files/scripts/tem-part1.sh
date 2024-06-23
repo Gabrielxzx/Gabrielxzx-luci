d32017ed2433b510bb5dad47ada6fdd3a923ec73
DESKTOP-ZX\10604 <gabrielx_zx@163.com>
2024-06-23 13:14:13 +0800

    refresh tem.sh

--- files/scripts/tem-part1.sh
+++ files/scripts/tem-part1.sh
@@ -2,8 +2,5 @@
 
 echo "This is tem_script_part1 start"
 
-rm -rf feeds/packages/net/uugamebooster/Makefile
-wget -qO feeds/packages/net/uugamebooster/Makefile https://raw.githubusercontent.com/breakings/OpenWrt/main/general/uugamebooster/Makefile
-
 
 echo "This is tem_script_part1 finish"
--- files/scripts/tem-part2.sh
+++ files/scripts/tem-part2.sh
@@ -2,5 +2,6 @@
 
 echo "This is tem_script_part2 start"
 
+sed -i '/luci-app-uugamebooster=y/d' .config
 
 echo "This is tem_script_part2 finish"
