b5f6ff99154d7e3dc9bf6ccaf9f95382bdd1b313
DESKTOP-ZX\10604 <gabrielx_zx@163.com>
2024-06-29 11:04:33 +0800

    add uugamebooster core files

--- files/scripts/tem-part2.sh
+++ files/scripts/tem-part2.sh
@@ -2,6 +2,7 @@
 
 echo "This is tem_script_part2 start"
 
-sed -i '/luci-app-uugamebooster=y/d' .config
+# sed -i '/luci-app-uugamebooster=y/d' .config
+sed -i "s/uu.gdl.netease.com\/uuplugin\/openwrt-/raw.githubusercontent.com\/Gabrielxzx\/Gabrielxzx-luci\/master\/files/uugamebooster\/core\//g" feeds/packages/net/uugamebooster/Makefile
 
 echo "This is tem_script_part2 finish"
