65de89ca2ca3bdbc25f15be80a1dcea53c7612d6
DESKTOP-ZX\10604 <gabrielx_zx@163.com>
2024-07-07 08:49:58 +0800

    Revert "cancel uugamebooster"
    This reverts commit 9c150c26fc3e336f10e881d34bc5f863976e47a4.

--- files/scripts/tem-part2.sh
+++ files/scripts/tem-part2.sh
@@ -3,6 +3,6 @@
 echo "This is tem_script_part2 start"
 
 # sed -i '/luci-app-uugamebooster=y/d' .config
-# sed -i "s/uu.gdl.netease.com\/uuplugin\/openwrt-/raw.githubusercontent.com\/Gabrielxzx\/Gabrielxzx-luci\/master\/files\/uugamebooster\/core\//g" feeds/packages/net/uugamebooster/Makefile
+sed -i "s/uu.gdl.netease.com\/uuplugin\/openwrt-/raw.githubusercontent.com\/Gabrielxzx\/Gabrielxzx-luci\/master\/files\/uugamebooster\/core\//g" feeds/packages/net/uugamebooster/Makefile
 
 echo "This is tem_script_part2 finish"
